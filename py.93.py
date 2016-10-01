928a39096f9f627c6bf1cc8f320d52150463321c
tarunsingh <singhtarun782@gmail.com>
2016-10-01 12:59:43 +0530

    Added odd/even

---
+++ py.93.py
@@ -0,0 +1,8 @@
+# program to identify the even/odd state of given number
+
+number = int(input("enter a number: "))
+
+if number % 2 ==0:
+    print ("the entered number is even")
+else:
+    print("the entered number is odd")
